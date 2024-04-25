2e30ddc9a9fb61e48c9a43720c2ea0a1ac5fbbba
nivashko <nivashko@gmail.com>
2024-04-25 14:38:25 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,3 @@
 <script>
-const Http = new XMLHttpRequest();
-const url='https://m7u714i8q0aar7ywb12bejlee5kw8swh.oastify.com/test';
-Http.open("GET", url);
-Http.send();
+alert(document.domain)
 </script>
